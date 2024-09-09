757b032adcacda57ba3bf1eb75051e8400b74d1b
amin <aminibrahim25@outlook.com>
2024-09-08 21:56:19 -0400

    first commit

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,95 +1,106 @@
-
-
-let productsHTML = ''
-products.forEach((product)=>{
-
-    let html = `
-        <div class="product-container">
-        <div class="product-image-container">
+let productsHTML = '';
+products.forEach((product) => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
         <img class="product-image"
-            src="${product.image}">
-        </div>
-
-        <div class="product-name limit-text-to-2-lines">
+          src="${product.image}">
+      </div>
+      <div class="product-name limit-text-to-2-lines">
         ${product.name}
-        </div>
-
-        <div class="product-rating-container">
+      </div>
+      <div class="product-rating-container">
         <img class="product-rating-stars"
-            src="images/ratings/rating-${product.rating.stars * 10}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
-            ${product.rating.count}
+          ${product.rating.count}
         </div>
-        </div>
-
-        <div class="product-price">
-        ${(product.priceCents / 100).toFixed(2)}
-        </div>
-
-        <div class="product-quantity-container">
-        <select>
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
+      </div>
+      <div class="product-price">
+        $${(product.priceCents / 100).toFixed(2)}
+      </div>
+      <div class="product-quantity-container">
+        <select class="js-quantity-selector-${product.id}">
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
         </select>
-        </div>
-
-        <div class="product-spacer"></div>
-
-        <div class="added-to-cart">
+      </div>
+      <div class="product-spacer"></div>
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
-        </div>
-
-        <button data-product-name="${product.name}" class="add-to-cart-button button-primary js-add-to-cart">
+      </div>
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
         Add to Cart
-        </button>
+      </button>
     </div>
-    `
-    productsHTML+=html
-    document.querySelector(`.js-products-grid`).innerHTML = productsHTML
-})
+  `;
+});
+const addedMessageTimeouts = {};
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+document.querySelectorAll('.js-add-to-cart')
 
-document.querySelectorAll(`.js-add-to-cart`)
-.forEach((button)=>{
-   button.addEventListener(`click`,()=>{
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      let matchingItem;
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+      const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+      
+      const quantity = Number(quantitySelector.value);
 
-      let productName =  button.dataset.productName
+      if (matchingItem) {
+        
+        matchingItem.quantity += quantity;
+      } else {
+        cart.push({
+          productId: productId,
+         
+          quantity: quantity
+        });
+      }
 
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
-   let matchItem
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
 
-   cart.forEach((item)=>{
-     if(productName === item.productName){
-          matchItem = item
-     }
-   })
-  
+      addedMessage.classList.add('added-to-cart-visible');
 
-   if(matchItem){
-     matchItem.quantity+= 1
-   }
-   else{
-    cart.push({
-        productName: productName,
-        quantity: 1
-      })
-   }
+      
+      
+      const previousTimeoutId = addedMessageTimeouts[productId];
 
-   let cartQuantity = 0
-   cart.forEach((item)=>{
-     cartQuantity += item.quantity
-   })
+      if (previousTimeoutId) {
+        clearTimeout(previousTimeoutId);
+      }
 
- document.querySelector(`.js-cartQuantity`).innerHTML =  cartQuantity
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
+      
 
-   }) 
-})
+      addedMessageTimeouts[productId] = timeoutId;
+    });
+  });
