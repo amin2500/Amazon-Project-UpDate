a07cbf828285d1ac3a7a51e725d20a0a7a754327
amin <aminibrahim25@outlook.com>
2024-10-02 18:43:53 -0400

    first commit

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,1 +1,48 @@
-let cart = []
+export let cart = [{
+    productId:`e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,
+    quantity:2
+},{
+    productId:`15b6fc6f-327a-4ec4-896f-486349e85a3d`,
+    quantity:2
+},{
+  productId:`id1`,
+  quantity:2
+}]
+
+export function addtocart(productId){
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+  
+  
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    const quantity = Number(quantitySelector.value);
+  
+  
+    if (matchingItem) {
+      
+      matchingItem.quantity += quantity;
+  
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: quantity
+      });
+    }
+  }
+
+
+  
+ export function removeFromCart(productId){
+    let newCart = []
+    
+    cart.forEach((cartItem)=>{
+      if(cartItem.productId !== productId){
+        newCart.push(cartItem)
+      }
+    })
+    cart = newCart
+  }
